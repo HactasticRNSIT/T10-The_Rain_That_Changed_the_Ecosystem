b358d0bc603fc08be4abf0157fd0e6dd898f4541
amulkumar056-beep <amulkumar056@gmail.com>
2026-05-08 17:33:46 +0530

    Delete src directory

--- src/lib/eco.functions.ts
+++
@@ -1,6 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-import { compute, type EcoInputs } from "./resilience-engine";
-
-export const predictEco = createServerFn({ method: "POST" })
-  .inputValidator((d: EcoInputs) => d)
-  .handler(async ({ data }) => compute(data));
